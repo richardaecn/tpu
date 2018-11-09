a96ea80e092188e9fdd967e920af2d0a25ef2fa6
richardaecn <richardaecn@github.com>
2018-11-09 14:28:03 +0000

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-STORAGE_BUCKET=gs://tpu_training
-DATA_DIR=${STORAGE_BUCKET}/data/ILSVRC2012/
+export PYTHONPATH="$PYTHONPATH:/home/yincui/tpu/models"
 
-python3 ./tpu/models/official/resnet/resnet_main.py \
-  --tpu=${TPU_NAME} \
+STORAGE_BUCKET=gs://tpu_training
+DATA_DIR=${STORAGE_BUCKET}/data/ILSVRC2012
+MODEL_DIR=${STORAGE_BUCKET}/resnet50_softmax
+ITER_PER_LOOP=200
+
+python /usr/share/tpu/models/official/resnet/resnet_main.py \
+  --tpu=$TPU_NAME \
   --data_dir=${DATA_DIR} \
-  --model_dir=${STORAGE_BUCKET}/resnet50_softmax
+  --model_dir=${MODEL_DIR} \
+  --iterations_per_loop=${ITER_PER_LOOP}
